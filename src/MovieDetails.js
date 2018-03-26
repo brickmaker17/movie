5d00cf6f4887853eead257b2d45566a787fa1847
Luke Barrett <gotluke@gmail.com>
2018-03-26 17:55:50 -0600

    styled details to the portals

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -4,15 +4,66 @@ import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
 const Modal = styled.div`
-    background-color: rgba(0,0,0,0.5);
+    background-color: rgba(255,255,255,1.0);
     position: fixed;
     height: 100%;
     width: 100%;
     top: 0;
     left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: 340px auto 1fr;
+    grid-template-rows: auto;
+    grid-template-areas: 
+        "heading ."
+        "image actors"
+        "image director"
+        "image writer"
+        "image score"
+        "image rating"
+        "image awards"
+        "image runtime"
+        "image production"
+        "image realease"
+        "plot plot"
+    
+`
+const Rate = styled.p`
+    grid-area: rating;
+`
+const Run = styled.p`
+    grid-area: runtime;
+    justify-self: start;
+    
+`
+const Rating = styled.p`
+    grid-area: runtime;
+    justify-self: center;
+`
+const Write = styled.p`
+    grid-area: writer;
+`
+const Heading = styled.h1`
+    grid-area: heading;
+    margin: 20px;
+`
+const Score = styled.p`
+    grid-area: score;
+`
+const Plots = styled.p`
+    grid-area: plot;
+`
+const Directors = styled.p`
+    grid-area: director;
+`
+const Award = styled.p`
+    grid-area: awards;
+`
+const Image = styled.img`
+    grid-area: image;
+    margin: 20px;
+`
+const Actor = styled.h4`
+    grid-area: actors;
 `
 const Close = styled.a`
     position: fixed;
@@ -20,6 +71,12 @@ const Close = styled.a`
     top: 0;
     right: 0;
     text-decoration: none;
+`
+const Prod = styled.p`
+    grid-area: production;
+`
+const Release = styled.p`
+    grid-area: realease
 `
 
 
@@ -57,20 +114,19 @@ class MovieDetails extends Component {
             ReactDOM.createPortal(
                 <Modal>
                     <Close href="#" onClick={this.props.click}>X</Close>
-                    <h1>{ Title }</h1>
-                    <img src={ Poster } alt={ Title }/>
-                    <h4>{ Actors }</h4>
-                    <p>{ Awards }</p>
-                    <p>{ Director }</p>
-                    <p>{ Plot }</p>
-                    <p>{ Metascore }</p>
-                    <p>{ Production }</p>
-                    <p>{ Rated }</p>
-                    <p>{ Released }</p>
-                    <p>{ Writer }</p>
-                    <p>{ Runtime }</p>
-                    <p>{ imdbRating }</p>
-
+                    <Heading>{ Title }</Heading>
+                    <Image src={ Poster } alt={ Title }/>
+                    <Actor>Actors: { Actors }</Actor>
+                    <Award>Awards: { Awards }</Award>
+                    <Directors>Director: { Director }</Directors>
+                    <Score>Metascore: { Metascore }</Score>
+                    <Prod>Production: { Production }</Prod>
+                    <Release>Released: { Released }</Release>
+                    <Write>Writers: { Writer }</Write>
+                    <Run>Runtime: { Runtime }</Run>
+                    <Rating>Rated: { Rated }</Rating>
+                    <Rate>IMDB Rating: { imdbRating }</Rate>
+                    <Plots>{ Plot }</Plots>
                 </Modal>,
                 // this.props.children,
                 document.getElementById('modal-root')
